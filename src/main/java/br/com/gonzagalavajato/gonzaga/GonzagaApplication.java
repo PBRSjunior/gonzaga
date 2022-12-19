3b65eb4e0da2820116a4044295fa4c73dfd8ab4f
paulo <pbrsj1385@gmail.com>
2022-12-19 11:58:41 -0300

    inicia projeto

---
+++ src/main/java/br/com/gonzagalavajato/gonzaga/GonzagaApplication.java
@@ -0,0 +1,13 @@
+package br.com.gonzagalavajato.gonzaga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GonzagaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GonzagaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gonzagalavajato/gonzaga/GonzagaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gonzagalavajato.gonzaga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GonzagaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
