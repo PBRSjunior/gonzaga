6755467ea8f1245e3d8314fe168618055ece6212
paulo <pbrsj1385@gmail.com>
2022-12-19 12:42:43 -0300

    altera caminho feature

--- src/main/java/br/com/gonzagalavajato/gonzaga/GonzagaApplication.java
+++ src/main/java/br/com/gonzagalavajato/gonzaga/GonzagaApplication.java
@@ -2,10 +2,15 @@ package br.com.gonzagalavajato.gonzaga;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping
 public class GonzagaApplication {
-
+	@GetMapping
 	public static void main(String[] args) {
 		SpringApplication.run(GonzagaApplication.class, args);
 	}
